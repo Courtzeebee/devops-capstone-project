3e1db94b68ff57225feb3a0693f14bfc66eeb7fd
Courtzeebee <135243674+Courtzeebee@users.noreply.github.com>
2023-11-06 07:11:55 -0500

    test cases and REST API for listing accounts

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -141,3 +141,10 @@ class TestAccountService(TestCase):
         )
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
         
+    def test_get_account_list(self):
+        """It should get a list of accounts"""
+        self._create_accounts(5)
+        resp = self.client.get(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), 5)
