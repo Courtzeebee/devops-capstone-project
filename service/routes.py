675ef2dc9cffb33c86f8e68f99706a9c644820d1
Courtzeebee <135243674+Courtzeebee@users.noreply.github.com>
2023-11-08 04:18:44 -0500

    fixing lint error

--- service/routes.py
+++ service/routes.py
@@ -30,7 +30,7 @@ def index():
             name="Account REST API Service",
             version="1.0",
             # paths=url_for("list_accounts", _external=True),
-        ),
+        ) ,
         status.HTTP_200_OK,
     )
 
@@ -62,7 +62,9 @@ def create_accounts():
 ######################################################################
 
 # ... place you code here to LIST accounts ...
-@app.route("/accounts",methods=["GET"])
+
+@app.route("/accounts", methods=["GET"])
+
 def list_accounts():
     """
     List all accounts
@@ -80,7 +82,9 @@ def list_accounts():
 ######################################################################
 
 # ... place you code here to READ an account ...
+
 @app.route("/accounts/<int:account_id>", methods=["GET"])
+
 def read_account(account_id):
     """Reads an account"""
     app.logger.info("Request to read an account with id: %s", account_id)
@@ -95,12 +99,15 @@ def read_account(account_id):
 ######################################################################
 
 # ... place you code here to UPDATE an account ...
+
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
+
 def update_accounts(account_id):
     """
     Update an account
     This endpoint will update an account based on the posted data
     """
+
     app.logger.info("Request to update an account with id: %s", account_id)
 
     account = Account.find(account_id)
@@ -117,15 +124,17 @@ def update_accounts(account_id):
 ######################################################################
 
 # ... place you code here to DELETE an account ...
-@app.route("/accounts/<int:account_id>", methods=["DELETE"])
-def delete_accounts(account_id):
-        """
-        Delete an Account
-        This endpoint will delete an Account based on the account_id that is requested
-        """
-        app.logger.info("Request to delete an Account with id: %s", account_id)
 
-        account = Account.find(account_id)
+@app.route("/accounts/<int:account_id>", methods=["DELETE"])
+
+def delete_accounts(account_id):
+    """
+    Delete an Account
+    This endpoint will delete an Account based on the account_id that is requested
+    """
+    app.logger.info("Request to delete an Account with id: %s", account_id)
+
+    account = Account.find(account_id)
         if account: 
             account.delete()
 
@@ -144,5 +153,5 @@ def check_content_type(media_type):
     app.logger.error("Invalid Content-Type: %s", content_type)
     abort(
         status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
-        f"Content-Type must be {media_type}",
+        f"Content-Type must be {media_type}", 
     )
